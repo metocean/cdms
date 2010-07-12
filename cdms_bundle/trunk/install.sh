9df6e73fe2078d732cee722dd6e7e6619580019d
frank <frank@364c883e-56ea-46cf-b203-9ad158a2e1dc>
2010-07-12 02:59:20 +0000

    moving into trunk

---
+++ cdms_bundle/trunk/install.sh
@@ -0,0 +1,42 @@
+#make sure zlib-devel and curl-devel are installed
+
+export CFLAGS=-fPIC
+export FC=gfortran
+export F77=gfortran
+export LDFLAGS=-lm
+
+tar -xzvf hdf*
+cd hdf*
+make clean
+./configure --prefix=/usr/local/HDF5 --enable-shared=no
+make
+make install
+cd ..
+
+tar -xzf netcdf*
+cd netcdf*
+make clean
+./configure --enable-netcdf-4 --with-hdf5=/usr/local/HDF5 --enable-cxx-4 --enable-dap
+make
+make install
+cd ..
+
+cd libcdms
+make clean
+./configure --enable-dap --with-ncinc=/usr/local/include --with-nclib=/usr/local/lib --prefix=/usr/local
+make
+make install
+cd ..
+
+easy_install -U mako
+easy_install -U numpy
+
+for package in cdtime cdms2 regrid2; do
+cd $package
+python setup.py build
+python setup.py install
+cd ..
+
+
+
+
