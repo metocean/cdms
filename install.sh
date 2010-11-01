b8bdef6b22d2eb4750ba4f915b5d3828883765b6
frank <frank@364c883e-56ea-46cf-b203-9ad158a2e1dc>
2010-11-01 02:21:59 +0000

    EXT-1 Merge support for grib2 to trunk

--- install.sh
+++ install.sh
@@ -1,36 +1,98 @@
-#!/bin/bash
+#
+# Dependencies
+#
+# Anything already installed will automatically be ignored
+# sudo yum install -y zlib-devel curl-devel python-mako
 
-#terminate on error
-set -e
 
-export CFLAGS=-fPIC
+
+
+build_dir=`pwd`/build
+install_dir=`pwd`/install #the actual place into which cdms is installed can be found at the end of the script
+
+
+#debugging: export CFLAGS="-fPIC -g "
+export CFLAGS="-fPIC"
 export FC=gfortran
 export F77=gfortran
+export LDFLAGS=-lm
 
-install_prefix=$PWD/install
-if [ "$UID" == "0" ]; then
-  install_prefix=/usr/local
-  # anything already installed will automatically ignored
-  yum install -y zlib-devel curl-devel netcdf netcdf-devel netcdf-static python-mako numpy
+
+
+mkdir -p $build_dir
+
+if [[ ! -s $install_dir/lib/libhdf5.a || ! -s $install_dir/include/H5FDcore.h ]]; then
+  echo "Installing HDF-5"
+  pushd $build_dir
+  tar -xzvf ../hdf5-1.8.3.tar.gz
+  cd hdf5-1.8.3
+  make clean
+  ./configure --prefix=$install_dir --enable-shared=no
+  make
+  make install
+  popd
 fi
 
-mkdir -p $install_prefix/bin
-mkdir -p $install_prefix/lib
-mkdir -p $install_prefix/include
-mkdir -p $install_prefix/man/man3
+
+if [[ ! -s $install_dir/lib/libnetcdf.a || ! -s $install_dir/include/netcdf.h ]]; then
+  echo "Installing netcdf"
+  pushd $build_dir
+  tar -xzf ../netcdf-4.1.1.tar.gz
+  cd netcdf-4.1.1
+  make clean
+  ./configure --prefix=$install_dir --enable-netcdf-4 --with-hdf5=$install_dir --enable-cxx-4 --enable-dap
+  make
+  make install
+  popd
+fi
+
+ 
+ 
+if [[ ! -s $install_dir/lib/libgrib2c.a || ! -s $install_dir/include/grib2.h ]]; then
+  echo "Installing grib2clib"
+  pushd $build_dir
+  tar -xzf ../g2clib-1.2.0.tar.gz
+  cd g2clib-1.2.0
+  patch -i ../../g2clib-1.2.0-makefile.patch
+  make all
+  mkdir -p $install_dir/lib/
+  cp libgrib2c.a $install_dir/lib/
+  mkdir -p $install_dir/include/
+  cp grib2.h $install_dir/include/
+  popd
+fi
+
 
 echo "Installing libcdms"
-cd libcdms
-make -i clean
-./configure --prefix=$install_prefix --disable-ql --disable-dap --disable-hdf --disable-drs --with-nclib=/usr/lib64 --with-ncinc=/usr/include
+pushd libcdms
+make distclean
+./configure CXX=g++ --enable-dap --prefix=$install_dir \
+            --with-ncinc=$install_dir/include --with-nclib=$install_dir/lib \
+            --with-hdflib=$install_dir/lib \
+            --with-grib2inc=$install_dir/include
 make
+mkdir --parents $install_dir/man/man3
 make install
-cd ..
+popd
 
+
+python_version=$(python --version 2>&1 | grep -E -o "[0-9]\.[0-9]")
+python_lib_path=$install_dir/lib/python$python_version/site-packages
+export PYTHONPATH=$python_lib_path
+mkdir -p $python_lib_path
+
+if [[ ! -s $python_lib_path/numpy-1.4.1-py$python_version-linux-x86_64.egg ]]; then
+  echo "Installing numpy"
+  easy_install --prefix $install_dir numpy-1.4.1.tar.gz
+fi
 
 for package in cdtime cdms2 regrid2; do
   echo "Installing $package"
   cd $package
-  python setup.py install --prefix=$install_prefix
+  rm -rf build
+  python setup.py install --force --prefix=$install_dir --install-lib=$install_dir/lib/python$python_version/site-packages
   cd ..
 done
+
+
+sudo cp -r  install/lib/python$python_version/site-packages/* /usr/lib64/python$python_version/site-packages/
